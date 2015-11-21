c88b48f8841069597cb471042e02ea48c5846993
Kalle Blomström <kalle.blomstrom@gmail.com>
2015-11-22 00:22:29 +0100

    Adds first version of search-map generation

--- src/clj_leaflet/core.clj
+++ src/clj_leaflet/core.clj
@@ -1,17 +1,35 @@
-(ns clj-leaflet.core)
+(ns clj-leaflet.core
+  (require [clojure.string :as s])
+  )
+
+
+(def noisy-words ["the" "featuring" "feat." "ft." "f/" "&" "Radio edit"])
+
+(defn in?
+ "Returns true if item i is in collection coll" 
+ [coll i]
+ (some #(= i %) coll))
+
+(defn remove-words
+  [ss]
+  (remove #(in? noisy-words %) ss))
 
 (defn clean-up
   "Cleans string s by removing words not interesting during search.
   (e.g. The and featuring variations."
-  [s]
-  () 
-  )
+  [search-string]
+  (let [dirty-string  (s/split (s/lower-case search-string) #" ")]
+  (->>
+    dirty-string
+   remove-words
+   (s/join " "))))
 
 (defn get-search-map
-  "Takes search arguments in the shape of artist a, song s and other o and returns a map with keys :artist, :song, :other, :search-string where :search-string is all variables as one string."
-  [artist track album]
-  (let [artist (clean-up a)
-        track r 
-        other (clean-up o)
-        ]) 
-  )
+  "Takes search arguments in the shape of artist a, song s and other o and returns a map with keys :artist, :song, :other, :search-string where :search-string is all variables as one string in the shape of artist song other."
+  ([artist track]
+   (get-search-map artist track ""))
+  ([artist  track other]
+  {:artist artist
+   :track track
+   :other (if (= "" other) nil other) 
+   :search-string (s/join " " (map clean-up [artist track other]))}))
