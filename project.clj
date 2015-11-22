ec2c61e7f200014e2f844d56fad8baed1c3e1832
Kalle Blomström <kalle.blomstrom@gmail.com>
2015-11-22 01:10:47 +0100

    Typo

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-leaflet "0.1.0-SNAPSHOT"
-  :description "A search string generation library for use online music searches."
+  :description "A search string generation library for use with online music searches."
   :url "http://github.com/blmstrm/clj-leaflet"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
